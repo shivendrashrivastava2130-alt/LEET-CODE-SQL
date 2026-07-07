0f1f56460d8cfaaeb3c9b21e8449106b36580610
shivendrashrivastava2130-alt <shivendrashrivastava2130@gmail.com>
2026-07-08 05:03:40 +0530

    Time: 524 ms (64.63%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,9 @@
+               select p.project_id ,round(avg(e.experience_years),2) as average_years
+              from Project p 
+              join Employee e on e.employee_id=p.employee_id
+              group by project_id
+
+
+
+
+
