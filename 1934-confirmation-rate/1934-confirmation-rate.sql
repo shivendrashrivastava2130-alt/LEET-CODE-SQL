62a6e07ff1b67b25f4c2076fddd6423411843e6a
shivendrashrivastava2130-alt <shivendrashrivastava2130@gmail.com>
2026-07-07 23:55:08 +0530

    Time: 677 ms (65.88%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,7 +1,22 @@
+with cte as (
 select s.user_id ,
-ifnull(round(avg(if(c.action="confirmed",1,0)),2),0) as confirmation_rate 
-from Signups s 
-left join Confirmations c 
-on c.user_id=s.user_id
-group by s.user_id;
+sum(case when c.action="confirmed" then 1 else 0 end) as accept_count,
+count(c.action) as total
+from signups s
+left join Confirmations c on 
+c.user_id=s.user_id
+group by s.user_id
+)
+
+select user_id ,
+round(ifnull(accept_count/total,0),2) as confirmation_rate 
+from cte
+
+
+
+
+
+
+
+
 
