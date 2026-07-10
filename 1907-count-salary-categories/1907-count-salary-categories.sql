3f02b07b66f653259727d53aa50ad0e0b30c2e53
shivendrashrivastava2130-alt <shivendrashrivastava2130@gmail.com>
2026-07-11 01:33:36 +0530

    Time: 1602 ms (41.77%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,14 @@
+select'High Salary' as category, sum(case when income >50000 then 1 else 0 end) as accounts_count
+from Accounts 
+union all
+select "Low Salary" as category ,sum(case when income <20000 then 1 else 0 end) as accounts_count
+from Accounts
+union all
+select'Average Salary' as category, sum(case when income between  20000 and 50000 then 1 else 0 end) as accounts_count
+from Accounts
+
+
+
+
+
+
